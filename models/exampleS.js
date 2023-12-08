6e6b1da44ee7650c388428e9a7e9d721674490d8
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2023-12-08 14:16:13 +0530

    Feat: creating the exampleS model

--- models/exampleS.js
+++ models/exampleS.js
@@ -1,2 +1,25 @@
  const mongoose = require('mongoose') // creating a model to interact with database in a easy way
 
+const exampleSchema = new mongoose.Schema({ // creating a javascript object
+// this will have keys for all the idffernet properties of our exampleS
+name: {
+// define different properties our the schema
+type: String,
+required: true,
+},
+sendingTo: {
+    type: String,
+    required: true,
+},
+
+date: {
+type:Date,
+required: true,
+default: Date.now
+},
+
+})
+
+//export the schema
+module.exports =mongoose.model('Example', exampleSchema)
+// the reason why we need to have the 'model' property it allows us to interact with the database directly using the schema
--- routes/example.js
+++ routes/example.js
@@ -1,18 +1,47 @@
 const express = require('express')
 const router= express.Router()
+const Example = require('../models/exampleS')
 
 //Getting all resources
-router.get('/',(req, res) => {
-res.send('My name is Pramudi') // Send a string res(response ) in a format other than JSON 
+router.get('/',async (req, res) => {
+try{
+ const example = await Example.find()
+  // getting all the resources
+  res.json(example)
+} catch (err) {
+res.status(500).json({message: err.message})
+} 
+// Send a string res(response ) in a format other than JSON 
+//500 error on the server
+
 })
+
 //Getting one resource
 router.get('/:id',(req, res) => { //:id means parameter
 res.send(req.params.id) // Send a string res(response
 })
+
+
 //Creating one resource
-router.post('/',(req, res) => { 
+router.post('/', async (req, res) => { 
+    const example = new Example({
+        name: req.body.name,
+        sendingTo: req.body.sendingTo,
+    })
+
+    try{
+        const newExample = await example.save()
+        res.status(201).json({newExample})
+        //201 => succesful sent
+    }
+    catch (err) {
+    res.status(400).json({message: err.message})    
+    }
 
 })
+
+
+
 //Updating one resource
 router.patch('/:id ',(req, res) => { // if we use put then all of the information of the example will be upated
 
